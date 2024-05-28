a624e76bd313086a9d0878885758f6e0ee61a5c7
matddung <matddung1111@gmail.com>
2024-05-28 14:42:33 +0900

    Initial Settings

---
+++ src/test/java/com/studyjun/lotto/LottoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.studyjun.lotto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LottoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
